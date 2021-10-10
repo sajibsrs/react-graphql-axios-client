d29cf8188560f2e436339f8507c9855e3dde601e
Sajidur Rahman <sajibsrs@gmail.com>
2021-10-10 10:05:51 +0600

    fetch result from github graphql api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const axiosGithubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+  }
+});
+
+const GET_ORGANIZATION = `
+    {
+      user (login: "sajibsrs") {
+        email
+        repositories(first: 100) {
+          nodes{
+            nameWithOwner
+          }
+        }
+      }
+    }`;
 
 function App() {
+  const [data, setData] = useState({ repositories: null, errors: null });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onFetchFromGithub();
+  }, []);
+
+  const submit = event => event.preventDefault();
+  const handleChange = event => {
+    setData(event.target.value);
+  };
+
+  const onFetchFromGithub = () => {
+    axiosGithubGraphQL
+      .post('', { query: GET_ORGANIZATION })
+      .then(result => {
+        setLoading(false);
+        console.log(result.data);
+        setData({
+          repositories: result.data.data.user.repositories.nodes,
+          errors: result,
+        })
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Github GraphQL API</h1>
+      <form onSubmit={submit}>
+        <label htmlFor="url">
+          Show repositories for username
+        </label>
+        <input id="url" type="text" onChange={handleChange} style={{ width: '300px' }} />
+        <button type="submit">Search</button>
+      </form>
+      {loading ? <p>loading...</p> :
+        <div>
+          {data.repositories && data.repositories.map((k, i) => <p key={i}>{k.nameWithOwner}</p>)}
+        </div>
+      }
     </div>
   );
 }

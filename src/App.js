4fb874a27f0f0cfc0ea4053d36f7f731b9201e84
Sajidur Rahman <sajibsrs@gmail.com>
2021-10-10 15:12:16 +0600

    removed unused function

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const axiosGithubGraphQL = axios.create({
   }
 });
 
-const GET_ORGANIZATION = `
+const GET_REPOSITORIES = `
     {
       user (login: "sajibsrs") {
         email
@@ -24,19 +24,17 @@ const GET_ORGANIZATION = `
 function App() {
   const [data, setData] = useState({ repositories: null, errors: null });
   const [loading, setLoading] = useState(true);
+  const [input, setInput] = useState(null);
 
   useEffect(() => {
     onFetchFromGithub();
   }, []);
 
   const submit = event => event.preventDefault();
-  const handleChange = event => {
-    setData(event.target.value);
-  };
 
   const onFetchFromGithub = () => {
     axiosGithubGraphQL
-      .post('', { query: GET_ORGANIZATION })
+      .post('', { query: GET_REPOSITORIES })
       .then(result => {
         setLoading(false);
         console.log(result.data);
@@ -54,7 +52,7 @@ function App() {
         <label htmlFor="url">
           Show repositories for username
         </label>
-        <input id="url" type="text" onChange={handleChange} style={{ width: '300px' }} />
+        <input id="url" type="text" onChange={setInput} style={{ width: '300px' }} />
         <button type="submit">Search</button>
       </form>
       {loading ? <p>loading...</p> :
